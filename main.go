fbf2244424789b80be4b849ef5b59ac747bd53cd
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-02-26 02:03:06 +0530

    Added server implementation with the modules and required functions

--- main.go
+++ main.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"rootkill/genie/database"
+	"rootkill/genie/models"
+	"rootkill/genie/routes"
+)
+
+func main() {
+	// Initialize Database
+	var db, err = database.InitDb()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Run Database migrations for models
+	db.AutoMigrate(&models.User{}, &models.Post{})
+
+	// Initialize Gin router
+	fmt.Println("Initialized gin router")
+	router := gin.Default()
+
+	// API routes
+	fmt.Println("Attaching routes to the gin server")
+	routes.SetupRoutes(router, db)
+
+	// Start Gin Server
+	fmt.Println("Server started at localhost:8080")
+	router.Run(":8080")
+}
